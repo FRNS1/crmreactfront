1390ec238c891dd62390f42c45ca987a14125e40
Matheus Fernandes <matheus.fernandes@delta.nt>
2023-08-25 09:17:22 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <img className="deltaimg" src="deltalogologin.png" />
+          <br>
+          </br>
+          <label className="stringInput"> Usuário </label>
+            <input className="containerInput"
+            type="text"
+            />
+          <br>
+          </br>
+        <label className="stringInput"> Senha </label>
+            <input className="containerInput"
+            type="password"
+            />
+          <br> 
+          </br>
+        <button className="botaoEntrar" type="submit">
+          <text className="stringEntrar"> Entrar </text>
+        </button>
+        <a className="esqueceuSenha" href="https://testador.com.br"> Esqueceu sua senha? </a>
     </div>
   );
 }
