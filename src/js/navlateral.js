5e4d9b2262687233e69c464ef5e984b0d8922b2b
Matheus Fernandes <matheus.fernandes@delta.nt>
2023-08-28 15:02:55 -0300

    up

--- src/js/navlateral.js
+++ src/js/navlateral.js
@@ -1,21 +1,33 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Importe FontAwesomeIcon
+import { faChartLine } from '@fortawesome/free-solid-svg-icons'; // Importe o ícone de gráfico
+import { faIdBadge } from '@fortawesome/free-solid-svg-icons';
+import { faHandshake } from '@fortawesome/free-solid-svg-icons';
 import '../css/sidebar.css';
-
-//dashboard
-//pessoas
-//propostas
 
 function NavLateral() {
   return (
     <div className="sidebar">
-      <img className="deltalogoazul" src={require('../imgs/deltalogoazul.png')} />
+      <img className="deltalogoazul" src={require('../imgs/deltalogoazul.png')} alt="Logo" />
       <ul className="sidebar-menu">
-        <li>Dashboard</li>
-        <li>Pessoas</li>
-        <li>Propostas</li>
+          <li>
+          <FontAwesomeIcon className="icone" icon={faChartLine} />
+          Dashboard
+          </li>
+        <hr className='line'></hr>
+          <li>
+          <FontAwesomeIcon className="icone" icon={faIdBadge} />
+          Clientes
+          </li>
+        <hr className='line'></hr>
+          <li>
+          <FontAwesomeIcon className="icone" icon={faHandshake} />
+          Propostas
+          </li>
+        <hr className='line'></hr>
       </ul>
     </div>
   );
-};
-    
-    export {NavLateral};
+}
+
+export { NavLateral };
