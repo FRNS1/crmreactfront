443d5e88c4be7662892f6184bc9fdfb77245b022
Matheus Fernandes <matheus.fernandes@delta.nt>
2023-08-28 16:01:40 -0300

    up

--- src/js/navlateral.js
+++ src/js/navlateral.js
@@ -1,31 +1,35 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Importe FontAwesomeIcon
-import { faChartLine } from '@fortawesome/free-solid-svg-icons'; // Importe o ícone de gráfico
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChartLine } from '@fortawesome/free-solid-svg-icons';
 import { faIdBadge } from '@fortawesome/free-solid-svg-icons';
 import { faHandshake } from '@fortawesome/free-solid-svg-icons';
 import '../css/sidebar.css';
+import { NavSuperior } from '../js/navsuperior'; // Importe NavSuperior
 
 function NavLateral() {
   return (
-    <div className="sidebar">
-      <img className="deltalogoazul" src={require('../imgs/deltalogoazul.png')} alt="Logo" />
-      <ul className="sidebar-menu">
+    <div>
+      <NavSuperior /> {/* Use a função NavSuperior aqui */}
+      <div className="sidebar">
+        <img className="deltalogoazul" src={require('../imgs/deltalogoazul.png')} alt="Logo" />
+        <ul className="sidebar-menu">
           <li>
-          <FontAwesomeIcon className="icone" icon={faChartLine} />
-          Dashboard
+            <FontAwesomeIcon className="icone" icon={faChartLine} />
+            Dashboard
           </li>
-        <hr className='line'></hr>
+          <hr className='line'></hr>
           <li>
-          <FontAwesomeIcon className="icone" icon={faIdBadge} />
-          Clientes
+            <FontAwesomeIcon className="icone" icon={faIdBadge} />
+            Clientes
           </li>
-        <hr className='line'></hr>
+          <hr className='line'></hr>
           <li>
-          <FontAwesomeIcon className="icone" icon={faHandshake} />
-          Propostas
+            <FontAwesomeIcon className="icone" icon={faHandshake} />
+            Propostas
           </li>
-        <hr className='line'></hr>
-      </ul>
+          <hr className='line'></hr>
+        </ul>
+      </div>
     </div>
   );
 }
