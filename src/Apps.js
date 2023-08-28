8b26ff10c8f0ea4c7eaeec2d114aedd702b636b9
FRNS1 <fernandesjoao2009@hotmail.com>
2023-08-28 18:08:11 -0300

    up

--- src/Apps.js
+++ src/Apps.js
@@ -1,13 +1,76 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import {NavLateral} from './js/navlateral';
+import axios from 'axios';
 
 const ButtonPage = () => {
   const navigate = useNavigate();
 
-  const handleButtonClick = () => {
-    navigate('/navlateral');
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [logged, setLogged] = useState(false);
+  const [userFullName, setUserFullName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [token, setToken] = useState("");
+  const [erro, setErro] = useState("");
+
+  const handleErroChange = (erro) => {
+    setErro(erro);
+  }
+
+  const handleTokenChange = (token) => {
+    setToken(token);
+  }
+
+  const handleLoggedChange = () => {
+    setLogged(true);
+  }
+
+  const handleEmailChange = (email) => {
+    setUserEmail(email);
+  }
+
+  const handleUserFullNameChange = (name) => {
+    setUserFullName(name);
+  }
+
+  const handleUserChange = (event) => {
+    setUser(event.target.value);
+  }
+  
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  async function handleButtonClick() {
+    const apiUrl = "http://127.0.0.1:8080/api/v1/users/login";
+  
+    const requestData = {
+      username: `${user}`,
+      password: `${password}`
+    };
+  
+    const headers = {
+      "Content-Type": "application/json"
+    };
+  
+    try {
+      const response = await axios.post(apiUrl, requestData, { headers });
+      const result = response.data;
+      handleLoggedChange();
+      handleTokenChange(result.token);
+      handleEmailChange(result.email);
+      handleUserFullNameChange(result.nome);
+      navigate('/navlateral');
+    } catch (error) {
+      handleErroChange("Usuário ou senha incorretos.")
+    }
   };
+ 
+
+  function test(){
+    console.log(token, userEmail);
+  }
 
   const handleEnterKeyPress = (event) => {
     if (event.key === 'Enter') {
@@ -15,6 +78,7 @@ const ButtonPage = () => {
       handleButtonClick();
     }
   };
+
 
   useEffect(() => {
     window.addEventListener('keydown', handleEnterKeyPress);
@@ -26,17 +90,18 @@ const ButtonPage = () => {
 
   return (
     <div className="container">
-      <img className="deltaimg" src={require('./imgs/deltalogologin.png')} alt="Logo" />
+      <img className="deltaimg" src={require('./imgs/deltalogologin.png')} alt="Logo" onClick={test} />
       <br />
       <label className="stringInput"> Usuário </label>
-      <input className="containerInput" type="text" />
+      <input className="containerInput" type="text" id="user" value={user} onChange={handleUserChange} />
       <br />
       <label className="stringInput"> Senha </label>
-      <input className="containerInput" type="password" />
+      <input className="containerInput" type="password" id="password" value={password} onChange={handlePasswordChange} />
       <br />
       <div>
         <button className="botaoEntrar" onClick={handleButtonClick}> Entrar </button>
       </div>
+      <text className='erro'>{erro}</text>
       <a className="esqueceuSenha" href="https://testador.com.br"> Esqueceu sua senha? </a>
     </div>
   );
