aa2b27b82146ecb0de52d8c8692168b050e88705
FRNS1 <fernandesjoao2009@hotmail.com>
2023-08-29 13:00:49 -0300

    up

--- src/js/navlateral.js
+++ src/js/navlateral.js
@@ -4,43 +4,31 @@ import { faChartLine } from '@fortawesome/free-solid-svg-icons';
 import { faIdBadge } from '@fortawesome/free-solid-svg-icons';
 import { faHandshake } from '@fortawesome/free-solid-svg-icons';
 import '../css/sidebar.css';
-<<<<<<< HEAD
+
+
 
 function NavLateral() {
   return (
-=======
-import { NavSuperior } from '../js/navsuperior';
-import { Helmet } from 'react-helmet';
-
-function NavLateral() {
-  return (
-    <div>
-      <NavSuperior />
-      <Helmet>
-        <link rel="icon" type="image/x-icon" href={require('../imgs/deltalogoazul.png')} />
-        <title>Teste</title>
-      </Helmet>
->>>>>>> 145acb5e8fa4956e460416959e95a0ed993be078
-      <div className="sidebar">
-        <img className="deltalogoazul" src={require('../imgs/deltalogoazul.png')} alt="Logo" />
-        <ul className="sidebar-menu">
-          <li className="sidebar-item">
-            <FontAwesomeIcon className="icone" icon={faChartLine} />
-            <text className='item-text'>Dashboard</text>
-          </li>
-          <hr className='line'></hr>
-          <li className="sidebar-item">
-            <FontAwesomeIcon className="icone" icon={faIdBadge} />
-            <text className='item-text'>Clientes</text>
-          </li>
-          <hr className='line'></hr>
-          <li className="sidebar-item">
-            <FontAwesomeIcon className="icone" icon={faHandshake} />
-            <text className='item-text'>Propostas</text>
-          </li>
-          <hr className='line'></hr>
-        </ul>
-      </div>
+        <div className="sidebar">
+          <img className="deltalogoazul" src={require('../imgs/deltalogoazul.png')} alt="Logo" />
+          <ul className="sidebar-menu">
+            <li className="sidebar-item">
+              <FontAwesomeIcon className="icone" icon={faChartLine} />
+              <text className='item-text'>Dashboard</text>
+            </li>
+            <hr className='line'></hr>
+            <li className="sidebar-item">
+              <FontAwesomeIcon className="icone" icon={faIdBadge} />
+              <text className='item-text'>Clientes</text>
+            </li>
+            <hr className='line'></hr>
+            <li className="sidebar-item">
+              <FontAwesomeIcon className="icone" icon={faHandshake} />
+              <text className='item-text'>Propostas</text>
+            </li>
+            <hr className='line'></hr>
+          </ul>
+        </div>
   );
 }
 
