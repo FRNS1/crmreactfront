f7e9b618d88c591f844b969e9a7a07d128a17332
FRNS1 <fernandesjoao2009@hotmail.com>
2023-08-29 10:27:42 -0300

    up

--- src/js/navlateral.js
+++ src/js/navlateral.js
@@ -4,28 +4,33 @@ import { faChartLine } from '@fortawesome/free-solid-svg-icons';
 import { faIdBadge } from '@fortawesome/free-solid-svg-icons';
 import { faHandshake } from '@fortawesome/free-solid-svg-icons';
 import '../css/sidebar.css';
-import { NavSuperior } from '../js/navsuperior'; // Importe NavSuperior
+import { NavSuperior } from '../js/navsuperior';
+import { Helmet } from 'react-helmet';
 
 function NavLateral() {
   return (
     <div>
-      <NavSuperior /> {/* Use a função NavSuperior aqui */}
+      <NavSuperior />
+      <Helmet>
+        <link rel="icon" type="image/x-icon" href={require('../imgs/deltalogoazul.png')} />
+        <title>Teste</title>
+      </Helmet>
       <div className="sidebar">
         <img className="deltalogoazul" src={require('../imgs/deltalogoazul.png')} alt="Logo" />
         <ul className="sidebar-menu">
           <li className="sidebar-item">
             <FontAwesomeIcon className="icone" icon={faChartLine} />
-            Dashboard
+            <text className='item-text'>Dashboard</text>
           </li>
           <hr className='line'></hr>
           <li className="sidebar-item">
             <FontAwesomeIcon className="icone" icon={faIdBadge} />
-            Clientes
+            <text className='item-text'>Clientes</text>
           </li>
           <hr className='line'></hr>
           <li className="sidebar-item">
             <FontAwesomeIcon className="icone" icon={faHandshake} />
-            Propostas
+            <text className='item-text'>Propostas</text>
           </li>
           <hr className='line'></hr>
         </ul>
