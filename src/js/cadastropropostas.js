6d3e8b6ca4f3b41e363ea650a731065c14f933da
FRNS1 <fernandesjoao2009@hotmail.com>
2023-09-21 11:14:45 -0300

    up

--- src/js/cadastropropostas.js
+++ src/js/cadastropropostas.js
@@ -3,9 +3,42 @@ import '../css/cadastro_propostas.css';
 import { NavSuperior } from '../js/navsuperior';
 import { NavLateral } from '../js/navlateral';
 import CurrencyInput from 'react-currency-input-field';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import axios from 'axios';
+
 
 function CadastroPropostas() {
-    const [valorDesejado, setValorDesejado] = React.useState('');
+    const navigate = useNavigate();
+    const [valorDesejado, setValorDesejado] = useState('');
+    const [prazo, setPrazo] = useState('');
+    const [obsAnalista, setObsAnalista] = useState('');
+
+    async function registerProposal(){
+        try{
+            const response = await axios.get("http://35.175.231.117:8080/api/v1/proposal/register", {
+                customer_id: Cookies.get('clienteSelecionado'),
+                user_id: Cookies.get('userid'),
+                valor_desejado: valorDesejado,
+                prazo: prazo,
+                observacao_cliente: obsAnalista
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${Cookies.get('token')}`,
+                    'Content-Type': 'application/json'
+                }
+            })
+            if (response.data == 'Created'){
+                alert('Dados enviados com sucesso!')
+                navigate('/visualizacaoindividual')
+            }
+        }catch (error){
+            alert('Houve um erro na requisição');
+            console.log(error);
+        }
+    }
+
     const handleValorDesejadoChange = (value, name) => {
         setValorDesejado(value);
     };
@@ -40,7 +73,7 @@ function CadastroPropostas() {
                             </div>
                             <div className='divfield'>
                                 <label className="stringDados"> Prazo </label>
-                                <input className="inputCad" type="text" required />
+                                <input className="inputCad" type="text" value={prazo} onChange={(e) => setPrazo(e.target.value)} required />
                             </div>
                             <div className='divfield' style={{ opacity: 0 }} id="none">
                                 <label className="stringDados"> Observação Cliente </label>
@@ -54,9 +87,9 @@ function CadastroPropostas() {
                     <text> Observação Cliente </text>
                 </div>
                 <div className='divtextArea'>
-                    <textarea className='textArea'> </textarea>
+                    <textarea className='textArea' onChange={(e) => setObsAnalista(e.target.value)}> </textarea>
                 </div>
-                <div className='divbotaoEnviarDados'>
+                <div className='divbotaoEnviarDados' onClick={registerProposal}>
                     <button className='botaoEnviarDados'>
                         <span className='stringEnviarDados'> Enviar Dados </span>
                     </button>
