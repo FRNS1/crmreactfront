d805aba3638bffc066de018cf6f061ac11edaf76
FRNS1 <fernandesjoao2009@hotmail.com>
2023-10-05 18:47:51 -0300

    up

--- src/js/registropagamentoindividual.js
+++ src/js/registropagamentoindividual.js
@@ -15,6 +15,10 @@ function RegistroPagamentoIndividual() {
     const [muda, handleButtonClick] = useState('geral');
     const [loanDetails, setLoanDetails] = useState([])
     const [payments, setPayments] = useState([]);
+    const [vencimento, setVencimento] = useState('');
+    const [valorParcela, setValorParcela] = useState('');
+    const [pago, setPago] = useState('');
+    const [dataPagamento, setDataPagamento] = useState('');
 
     const Mudapagina = (value) => {
         handleButtonClick(value);
@@ -71,45 +75,141 @@ function RegistroPagamentoIndividual() {
             }
             const result = await response.json();
             setPayments(result);
+            setValorParcela(result.pagamento);
+            setPago(result.pago);
+            setDataPagamento(result.dataPagamento);
             console.log(result);
         } catch (error) {
             console.error('error', error);
         }
     }
 
-    async function sendData(id, valorParcela, pago, dataPagamento) {
-        let dataPagamentoUs;
-        try {
-            const dataPagamentoSplit = dataPagamento.split("/");
-            dataPagamentoUs = `${dataPagamentoSplit[2]}-${dataPagamentoSplit[1]}-${dataPagamentoSplit[0]}`;
-        } catch {
-            console.log("Não mudou a data");
-        }
-
-        const data = {
-            parcela: id,
-            valor_parcela: valorParcela.toString().replace("R$ ", ""),
-            pago: pago,
-            data_pagamento: dataPagamentoUs,
-        };
-
-        const urlPost = `http://35.175.231.117:8080/api/v1/payments/update`;
-
-        try {
-            const response = await axios.post(urlPost, data, {
+    async function sendVencimento(id, vencimento){
+        try{
+            const dataPagamentoSplit = vencimento.split("/");
+            let dataPagamentoUs = `${dataPagamentoSplit[2]}-${dataPagamentoSplit[1]}-${dataPagamentoSplit[0]}`;
+            const urlPost = `http://35.175.231.117:8080/api/v1/payments/update/vencimento`;
+            const response = await axios.post(urlPost, {
+                parcela: id,
+                vencimento: dataPagamentoUs
+            }, {
                 headers: {
                     Authorization: `Bearer ${Cookies.get('token')}`,
                     'Content-Type': 'application/json',
                 },
-            });
-
-            if (response.status === 200) {
-                alert("Dados atualizados");
+            })
+            if (response.status == 200){
+                alert("Dados atualizados com sucesso!");
             }
-        } catch (error) {
-            console.error("Erro ao enviar os dados:", error);
+        }catch (error){
+            console.log(error);
         }
     }
+
+    async function sendValorParcela(id, valorParcela){
+        try{
+            const urlPost = `http://35.175.231.117:8080/api/v1/payments/update/valorparcela`;
+            const response = await axios.post(urlPost, {
+                parcela: id,
+                valor_parcela: valorParcela.replace("R$ ", "").replace(",", ".")
+            }, {
+                headers: {
+                    Authorization: `Bearer ${Cookies.get('token')}`,
+                    'Content-Type': 'application/json',
+                },
+            })
+            if (response.status == 200){
+                alert("Dados atualizados!")
+            }
+        }catch (error){
+            console.log(error);
+        }
+    }
+
+    async function sendPago(id, pago){
+        try{
+            const urlPost = `http://35.175.231.117:8080/api/v1/payments/update/pago`;
+            const response = await axios.post(urlPost, {
+                parcela: id,
+                pago: pago
+            }, {
+                headers: {
+                    Authorization: `Bearer ${Cookies.get('token')}`,
+                },
+            })
+            if (response.status == 200){
+                alert("Dados atualizados!");
+            }
+        }catch (error){
+            console.log(error);
+        }
+    }
+
+    async function sendDataPagamento(id, dataPagamento){
+        try{
+            const dataPagamentoSplit = dataPagamento.split("/");
+            let dataPagamentoUs = `${dataPagamentoSplit[2]}-${dataPagamentoSplit[1]}-${dataPagamentoSplit[0]}`;
+            const urlPost = `http://35.175.231.117:8080/api/v1/payments/update/datapagamento`;
+            const response = await axios.post(urlPost, {
+                parcela: id,
+                data_pagamento: dataPagamentoUs
+            }, {
+                headers: {
+                    Authorization: `Bearer ${Cookies.get('token')}`,
+                    'Content-Type': 'application/json',
+                },
+            })
+            if (response.status == 200){
+                alert("Dados atualizados!");
+            }
+        }catch (error){
+            console.log(error);
+        }
+    }
+
+    // async function sendData(id) {
+    //     console.log(pago);
+    //     console.log(valorParcela);
+    //     console.log(dataPagamento);
+    //     let dataPagamentoUs;
+    //     try {
+    //         const dataPagamentoSplit = dataPagamento.split("/");
+    //         dataPagamentoUs = `${dataPagamentoSplit[2]}-${dataPagamentoSplit[1]}-${dataPagamentoSplit[0]}`;
+    //     } catch {
+    //         dataPagamentoUs = dataPagamento;
+    //         console.log("Não mudou a data");
+    //     }
+
+    //     try{
+    //         valorParcela = valorParcela.replace("R$ ", "").replace(",", ".");
+    //     } catch {
+    //         console.log("Valor indefinido");
+    //     }
+
+    //     const data = {
+    //         parcela: id,
+    //         valor_parcela: valorParcela.replace("R$ ", "").replace(",", "."),
+    //         pago: pago,
+    //         data_pagamento: dataPagamentoUs,
+    //     };
+
+    //     const urlPost = `http://35.175.231.117:8080/api/v1/payments/update`;
+
+    //     try {
+    //         const response = await axios.post(urlPost, data, {
+    //             headers: {
+    //                 Authorization: `Bearer ${Cookies.get('token')}`,
+    //                 'Content-Type': 'application/json',
+    //             },
+    //         });
+
+    //         if (response.status === 200) {
+    //             alert("Dados atualizados");
+    //         }
+    //     } catch (error) {
+    //         console.error("Erro ao enviar os dados:", error);
+    //     }
+    // }
 
     useEffect(() => {
         fetchLoanDetails();
@@ -206,79 +306,6 @@ function RegistroPagamentoIndividual() {
         }
     }
 
-    function Geral() {
-
-        const [paymentSelections, setPaymentSelections] = useState([]);
-
-        useEffect(() => {
-            // Inicialize o array de seleções com base na lista de pagamentos
-            const initialSelections = payments.map((payment) => payment.pago || 'Vingente');
-            setPaymentSelections(initialSelections);
-        }, [payments]);
-
-        const handlePagamentosChange = (event, paymentIndex) => {
-            const { value } = event.target;
-            setPaymentSelections((prevSelections) => {
-                const newSelections = [...prevSelections];
-                newSelections[paymentIndex] = value;
-                return newSelections;
-            });
-        };
-
-        return (
-            <div className='divtabelaPagamentosIndividual'>
-                <table className='tabelaPagamentosIndividual'>
-                    <thead>
-                        <tr>
-                            <th> Parcela </th>
-                            <th> Vencimento </th>
-                            <th> Saldo Devedor </th>
-                            <th> Amortização </th>
-                            <th> Juros </th>
-                            <th> Valor Parcela </th>
-                            <th> Pago? </th>
-                            <th> Data Pagamento </th>
-                            <th> Editar? </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {payments.map((payment, index) => (
-                            <tr key={payment.id}>
-                                <td> {payment.num_parcela} </td>
-                                <td> <InputMask mask="99/99/9999" placeholder="DD/MM/AAAA" type="text" className="inputDataPagamentos" value={payment.vencimento ? format(new Date(payment.vencimento), 'dd/MM/yyyy') : ''} disabled /> </td>
-                                <td> {`R$ ${payment.saldo_devedor}`} </td>
-                                <td> {payment.amortizacao} </td>
-                                <td> {`R$ ${payment.juros}`} </td>
-                                <td>
-                                    <CurrencyInput
-                                        prefix='R$ '
-                                        name="valorParcela"
-                                        type="text"
-                                        placeholder={`R$ ${payment.pagamento}`}
-                                        className="selectPago"
-                                    />
-                                </td>
-                                <td>
-                                    <select className='selectPago' value={paymentSelections[index]} onChange={(event) => handlePagamentosChange(event, index)}>
-                                        <option className='optionselectPago' value="VIGENTE"> VIGENTE </option>
-                                        <option className='optionselectPago' value="EM ATRASO"> EM ATRASO </option>
-                                        <option className='optionselectPago' value="PAGO"> PAGO </option>
-                                    </select>
-                                </td>
-                                <td> <InputMask mask="99/99/9999" type="text" className="inputDataPagamentos" placeholder={payment.data_pagamento ? format(new Date(payment.data_pagamento), 'dd/MM/yyyy') : ''} /> </td>
-                                <td>
-                                    <button className="botaoTDVer" style={{ backgroundColor: '#081535' }} onClick={() => sendData(payment.parcela_id, payment.pagamento, payment.pago, payment.data_pagamento)}>
-                                        <span className='stringVer' style={{ color: 'white' }}>salvar</span>
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        );
-    };
-
     return (
         <div className='containerPrincipal'>
             <div className='positionNavLateral'>
@@ -338,7 +365,58 @@ function RegistroPagamentoIndividual() {
                 </table>
                 <div>
                     {muda === 'geral' && (
-                        <Geral />
+                        <div className='divtabelaPagamentosIndividual'>
+                        <table className='tabelaPagamentosIndividual'>
+                            <thead>
+                                <tr>
+                                    <th> Parcela </th>
+                                    <th> Vencimento </th>
+                                    <th> Saldo Devedor </th>
+                                    <th> Amortização </th>
+                                    <th> Juros </th>
+                                    <th> Valor Parcela </th>
+                                    <th> Pago? </th>
+                                    <th> Data Pagamento </th>
+                                    <th> Editar? </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {payments.map((payment) => (
+                                    <tr key={payment.id}>
+                                        <td> {payment.num_parcela} </td>
+                                        <td> <InputMask mask="99/99/9999" onChange={(e) => sendVencimento(payment.parcela_id, e.target.value)} placeholder={payment.vencimento ? format(new Date(payment.vencimento), 'dd/MM/yyyy') : ''} type="text" className="inputDataPagamentos" /> </td>
+                                        <td> {`R$ ${payment.saldo_devedor}`} </td>
+                                        <td> {payment.amortizacao} </td>
+                                        <td> {`R$ ${payment.juros}`} </td>
+                                        <td>
+                                            <CurrencyInput
+                                                prefix='R$ '
+                                                name="valorParcela"
+                                                type="text"
+                                                placeholder={`R$ ${payment.pagamento}`}
+                                                className="selectPago"
+                                                onChange={(e) => sendValorParcela(payment.parcela_id, e.target.value)}
+                                            />
+                                        </td>
+                                        <td>
+                                            <select className='selectPago' onChange={(e) => sendPago(payment.parcela_id, e.target.value)}>
+                                                <option className='optionselectPago' value={payment.pago}> {payment.pago} </option>
+                                                <option className='optionselectPago' value="VIGENTE"> VIGENTE </option>
+                                                <option className='optionselectPago' value="EM ATRASO"> EM ATRASO </option>
+                                                <option className='optionselectPago' value="PAGO"> PAGO </option>
+                                            </select>
+                                        </td>
+                                        <td> <InputMask mask="99/99/9999" type="text" onChange={(e) => sendDataPagamento(payment.parcela_id, e.target.value)} className="inputDataPagamentos" placeholder={payment.data_pagamento ? format(new Date(payment.data_pagamento), 'dd/MM/yyyy') : ''} /> </td>
+                                        <td>
+                                            <button className="botaoTDVer" style={{ backgroundColor: '#081535' }}>
+                                                <span className='stringVer' style={{ color: 'white' }}>salvar</span>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                     )}
                     {muda === 'observacoes' && (
                         <Observacoes />
