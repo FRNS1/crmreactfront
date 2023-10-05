e82cf001ddf5579feec4f153c129bcd224d390c5
Matheus Fernandes <matheus.fernandes@delta.nt>
2023-10-05 15:35:45 -0300

    opções de cnaes no campo de segmento de checklist

--- src/components/pages/checklist/PJ/index.jsx
+++ src/components/pages/checklist/PJ/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Select from 'react-select';
 import CurrencyInput from 'react-currency-input-field';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
@@ -8,6 +9,72 @@ import Swal from 'sweetalert2'
 import '../style.css'
 import Loading from '../../../UI/Loading'
 import NewModal from "../../../UI/NewModal"
+
+const options = [
+    { value: 'opcao1', label: 'Seleciona uma opção:' },
+    { value: 'opcao2', label: 'Comércio varejista de artigos do vestuário e acessórios' },
+    { value: 'opcao3', label: 'Atividades de organizações políticas' },
+    { value: 'opcao4', label: 'Lanchonetes casas de chá de sucos e similares' },
+    { value: 'opcao5', label: 'Comércio varejista de mercadorias em geral com predominância de produtos alimentícios - minimercados mercearias e armazéns' },
+    { value: 'opcao6', label: 'Cabeleireiros manicure e pedicure' },
+    { value: 'opcao7', label: 'Restaurantes e similares' },
+    { value: 'opcao8', label: 'Obras de alvenaria' },
+    { value: 'opcao9', label: 'Atividades de associações de defesa de direitos sociais' },
+    { value: 'opcao10', label: 'Promoção de vendas' },
+    { value: 'opcao11', label: 'Comércio varejista de bebidas' },
+    { value: 'opcao12', label: 'Comércio varejista de artigos de óptica' },
+    { value: 'opcao13', label: 'Comércio varejista de outros produtos não especificados anteriormente' },
+    { value: 'opcao14', label: 'Transporte rodoviário de carga exceto produtos perigosos e mudanças intermunicipal interestadual e internacional' },
+    { value: 'opcao15', label: 'Confecção de peças do vestuário exceto roupas íntimas e as confeccionadas sob medida' },
+    { value: 'opcao16', label: 'Fornecimento de alimentos preparados preponderantemente para consumo domiciliar' },
+    { value: 'opcao17', label: 'Comércio varejista de cosméticos produtos de perfumaria e de higiene pessoal' },
+    { value: 'opcao18', label: 'Transporte rodoviário de carga exceto produtos perigosos e mudanças municipal' },
+    { value: 'opcao19', label: 'Bares e outros estabelecimentos especializados em servir bebidas sem entretenimento' },
+    { value: 'opcao20', label: 'Instalação e manutenção elétrica' },
+    { value: 'opcao21', label: 'Comércio a varejo de peças e acessórios novos para veículos automotores' },
+    { value: 'opcao22', label: 'Comércio varejista de mercadorias em geral com predominância de produtos alimentícios - hipermercados' },
+    { value: 'opcao23', label: 'Atividades de estética e outros serviços de cuidados com a beleza' },
+    { value: 'opcao24', label: 'Comércio varejista de produtos alimentícios em geral ou especializado em produtos alimentícios não especificados anteriormente' },
+    { value: 'opcao25', label: 'Serviços de manutenção e reparação mecânica de veículos automotores' },
+    { value: 'opcao26', label: 'Serviços ambulantes de alimentação' },
+    { value: 'opcao27', label: 'Construção de edifícios' },
+    { value: 'opcao28', label: 'Comércio varejista de produtos farmacêuticos sem manipulação de fórmulas' },
+    { value: 'opcao29', label: 'Comércio varejista de carnes - açougues' },
+    { value: 'opcao30', label: 'Comércio varejista de móveis' },
+    { value: 'opcao31', label: 'Serviços de organização de feiras congressos exposições e festas' },
+    { value: 'opcao32', label: 'Comércio varejista especializado de equipamentos e suprimentos de informática' },
+    { value: 'opcao33', label: 'Condomínios prediais' },
+    { value: 'opcao34', label: 'Comércio varejista de materiais de construção em geral' },
+    { value: 'opcao35', label: 'Preparação de documentos e serviços especializados de apoio administrativo não especificados anteriormente' },
+    { value: 'opcao36', label: 'Comércio varejista de ferragens e ferramentas' },
+    { value: 'opcao37', label: 'Atividades de organizações religiosas ou filosóficas' },
+    { value: 'opcao38', label: 'Outras atividades de serviços prestados principalmente às empresas não especificadas anteriormente' },
+    { value: 'opcao39', label: 'Reparação e manutenção de computadores e de equipamentos periféricos' },
+    { value: 'opcao40', label: 'Serviços de pintura de edifícios em geral' },
+    { value: 'opcao41', label: 'Outras atividades de ensino não especificadas anteriormente' },
+    { value: 'opcao42', label: 'Comercio varejista de artigos de armarinho' },
+    { value: 'opcao43', label: 'Serviços combinados de escritório e apoio administrativo' },
+    { value: 'opcao44', label: 'Comércio varejista de calçados' },
+    { value: 'opcao45', label: 'Treinamento em desenvolvimento profissional e gerencial' },
+    { value: 'opcao46', label: 'Padaria e confeitaria com predominância de revenda' },
+    { value: 'opcao47', label: 'Serviços de entrega rápida' },
+    { value: 'opcao48', label: 'Fabricação de móveis com predominância de madeira' },
+    { value: 'opcao49', label: 'Criação de bovinos para corte' },
+    { value: 'opcao50', label: 'Comércio varejista especializado de eletrodomésticos e equipamentos de áudio e vídeo' },
+    { value: 'opcao52', label: 'Consultoria em tecnologia da informação' },
+    { value: 'opcao53', label: 'Suporte técnico, manutenção e outros serviços em tecnologia da informação' },
+    { value: 'opcao54', label: 'Comércio atacadista de mercadorias em geral, sem predominância de alimentos ou de insumos agropecuários' },
+    { value: 'opcao55', label: 'Serviços de agronomia e de consultoria às atividades agrícolas e pecuárias' },
+    { value: 'opcao56', label: 'Educação infantil - creche' },
+    { value: 'opcao57', label: 'Educação infantil - pré-escola' },
+    { value: 'opcao58', label: 'Educação superior - graduação' },
+    { value: 'opcao59', label: 'Educação superior - graduação e pós-graduação' },
+    { value: 'opcao60', label: 'Educação superior - pós-graduação e extensão' },
+    { value: 'opcao61', label: 'Educação profissional de nível técnico' },
+    { value: 'opcao62', label: 'Educação profissional de nível tecnológico' },
+    { value: 'opcao63', label: 'Atividades de apoio à educação, exceto caixas escolares' },
+    { value: 'opcao64', label: 'Serviços de agronomia e de consultoria às atividades agrícolas e pecuárias' },
+]
 
 function ChecklistPj() {
     const navigate = useNavigate();
@@ -31,7 +98,7 @@ function ChecklistPj() {
     const toggleModalTerms = () => {
         setIsOpenModalTerms(!isOpenModalTerms)
     }
-    
+
     const toggleModalTermsScr = () => {
         setIsOpenModalTermsScr(!isOpenModalTermsScr)
     }
@@ -100,6 +167,10 @@ function ChecklistPj() {
         setValorDesejado(value);
     };
 
+    const handleChange = selectedOption => {
+        console.log(`Opção selecionada: ${selectedOption.label}`);
+    };
+
     return (
         <>
             <NewModal isOpen={isOpenModalTerms} onRequestClose={toggleModalTerms}>
@@ -127,55 +198,54 @@ function ChecklistPj() {
             </NewModal>
             <NewModal isOpen={isOpenModalTermsScr} onRequestClose={toggleModalTermsScr}>
                 <div className='modalcontainer'>
-                <h1>Termo de Autorização para Consulta SCR</h1><br></br><br></br>
-                <h2>COMUNICADO DE INCLUSÃO E AUTORIZAÇÃO
-                    PARA CONSULTA AO SISTEMA DE INFORMAÇÕES DE
-                    CRÉDITO DO BANCO CENTRAL DO BRASIL – SCR</h2><br></br><br></br>
-                Autorizo a UY3 Sociedade de Crédito Direto S.A. (“SCD”) em conjunto com BDI SERVICOS
-                FINANCEIROS
-                LTDA e seus parceiros de análise de credito a compartilhar e consultar os débitos e
-                responsabilidades
-                decorrentes de operações com característica de crédito e as informações e os
-                registros de medidas
-                judiciais que constem ou venham a constar em nome da {nomeSocio} e {cpfSocio} do
-                Sistema
-                de Informações de Crédito (SCR), gerido pelo Banco Central do Brasil - Bacen, ou dos
-                sistemas que venham
-                complementá-lo ou a substitui-lo.<br></br><br></br>
-                Estou ciente de que: a) o SCR tem por finalidades prover informações ao Banco
-                Central do Brasil, para
-                fins de monitoramento do crédito no sistema financeiro e para o exercício de suas
-                atividades de
-                fiscalização e propiciar o intercâmbio de informações entre instituições
-                financeiras, conforme definido
-                no § 1º do art. 1º da Lei Complementar nº 105, de 10 de janeiro de 2001, sobre o
-                montante de
-                responsabilidades de clientes em operações de crédito, com o objetivo de subsidiar
-                decisões de crédito
-                e de negócios; b) poderei ter acesso aos dados constantes em meu nome no SCR por
-                meio do sistema
-                registrado do Banco Central do Brasil – Bacen ou na Central de Atendimento ao
-                Público do Bacen; c) a
-                consulta sobre qualquer informação ao SCR depende de minha prévia autorização, que é
-                lavrada por
-                meio desse termo; d) as manifestações de discordância quanto às informações
-                constantes no SCR e os
-                pedidos de correção, exclusão e registro de medidas judiciais no SCR deverão ser
-                dirigidos a SCD, por
-                meio de requerimento escrito e fundamentado enviado pelo Cliente, acompanhado da
-                respectiva
-                decisão judicial, conforme aplicável.<br></br><br></br>
-                O Cliente declara estar de acordo com a formalização desta autorização por meio de
-                todas as formas
-                em direito admitidas, incluindo meios eletrônicos e digitais como válidos e
-                plenamente eficazes, ainda
-                que seja estabelecida assinatura e aceitação eletrônica ou certificação fora dos
-                padrões ICP – Brasil,
-                conforme disposto pelo art. 10 da Medida Provisória nº 2.200/2001 em vigor no
-                Brasil.<br></br><br></br>
+                    <h1>Termo de Autorização para Consulta SCR</h1><br></br><br></br>
+                    <h2>COMUNICADO DE INCLUSÃO E AUTORIZAÇÃO
+                        PARA CONSULTA AO SISTEMA DE INFORMAÇÕES DE
+                        CRÉDITO DO BANCO CENTRAL DO BRASIL – SCR</h2><br></br><br></br>
+                    Autorizo a UY3 Sociedade de Crédito Direto S.A. (“SCD”) em conjunto com BDI SERVICOS
+                    FINANCEIROS
+                    LTDA e seus parceiros de análise de credito a compartilhar e consultar os débitos e
+                    responsabilidades
+                    decorrentes de operações com característica de crédito e as informações e os
+                    registros de medidas
+                    judiciais que constem ou venham a constar em nome da {nomeSocio} e {cpfSocio} do
+                    Sistema
+                    de Informações de Crédito (SCR), gerido pelo Banco Central do Brasil - Bacen, ou dos
+                    sistemas que venham
+                    complementá-lo ou a substitui-lo.<br></br><br></br>
+                    Estou ciente de que: a) o SCR tem por finalidades prover informações ao Banco
+                    Central do Brasil, para
+                    fins de monitoramento do crédito no sistema financeiro e para o exercício de suas
+                    atividades de
+                    fiscalização e propiciar o intercâmbio de informações entre instituições
+                    financeiras, conforme definido
+                    no § 1º do art. 1º da Lei Complementar nº 105, de 10 de janeiro de 2001, sobre o
+                    montante de
+                    responsabilidades de clientes em operações de crédito, com o objetivo de subsidiar
+                    decisões de crédito
+                    e de negócios; b) poderei ter acesso aos dados constantes em meu nome no SCR por
+                    meio do sistema
+                    registrado do Banco Central do Brasil – Bacen ou na Central de Atendimento ao
+                    Público do Bacen; c) a
+                    consulta sobre qualquer informação ao SCR depende de minha prévia autorização, que é
+                    lavrada por
+                    meio desse termo; d) as manifestações de discordância quanto às informações
+                    constantes no SCR e os
+                    pedidos de correção, exclusão e registro de medidas judiciais no SCR deverão ser
+                    dirigidos a SCD, por
+                    meio de requerimento escrito e fundamentado enviado pelo Cliente, acompanhado da
+                    respectiva
+                    decisão judicial, conforme aplicável.<br></br><br></br>
+                    O Cliente declara estar de acordo com a formalização desta autorização por meio de
+                    todas as formas
+                    em direito admitidas, incluindo meios eletrônicos e digitais como válidos e
+                    plenamente eficazes, ainda
+                    que seja estabelecida assinatura e aceitação eletrônica ou certificação fora dos
+                    padrões ICP – Brasil,
+                    conforme disposto pelo art. 10 da Medida Provisória nº 2.200/2001 em vigor no
+                    Brasil.<br></br><br></br>
                 </div>
             </NewModal>
-
             <div className="containerchecklist">
                 <div className='contentchecklist'>
                     <img className="deltalogoazulchecklist" src='https://docsbora.s3.amazonaws.com/uploads/deltalogoazul.png' alt="Logo" />
@@ -217,13 +287,11 @@ function ChecklistPj() {
                         </div>
                         <div className="form-groupchecklist">
                             <label className='classNamechecklist'>Segmento</label>
-                            <input
-                                className='inputchecklist'
-                                type="text"
-                                value={segmento}
-                                onChange={(e) => setSegmento(e.target.value)}
-                                placeholder="Digite o segmento da empresa"
-                                required
+                            <Select
+                                options={options}
+                                isSearchable={true}
+                                placeholder="Digite para pesquisar..."
+                                onChange={handleChange}
                             />
                         </div>
                         <div className="form-groupchecklist">
@@ -329,7 +397,7 @@ function ChecklistPj() {
                         <div className='buttonchecklist' onClick={() => sendData()}>Enviar</div>
                     </form>
                 </div>
-                {isLoading && <Loading/>}
+                {isLoading && <Loading />}
             </div>
         </>
     )
