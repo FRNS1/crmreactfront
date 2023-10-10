d2cf9a215a7a8af786823c39f4998622f7c860d9
CatianoNorberto <catianonorberto@gmail.com>
2023-10-10 00:58:13 -0300

    Ajustando o menu e sidebar do dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,36 +1,60 @@
-// import HeaderDashboard from '../../components/Navsuperior'
-// import { DashboardContainer, DashboardContent, Content } from './style'
-// import { Layout } from 'antd'
-import { Layout, Menu, Button, theme } from 'antd'
+import React, { useState } from 'react'
+import { Layout, Button, theme } from 'antd'
+import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons'
 
 import NavSuperior from '../../components/Navsuperior'
 import Logo from '../../components/Logo'
 import Sidebar from '../../components/Sidebar'
+import ToggleTheme from '../../components/ToggleTheme'
 
 import './style.css'
 
 const { Header, Sider, Content} = Layout
 
 export default function Dashboard() {
+  const [darkTheme, setDarkTheme] = useState(true)
+  const [collapsed, setCollapsed] = useState(true)
   const {
     token: { colorBgContainer },
   } = theme.useToken();
 
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme)
+  }
   return(
     <Layout>
-      <Sider className='sidebarContainer'>
+      <Sider 
+        tigger={null}
+        collapsible
+        collapsed={collapsed}
+        className='sidebarContainer' 
+        theme={darkTheme ? 'light' : 'dark'}>
         <Logo/>
-        <Sidebar/>
+        <Sidebar darkTheme={darkTheme}/>
+        <ToggleTheme 
+          darkTheme={darkTheme}
+          toggleTheme={toggleTheme}
+        />
       </Sider>
       <Layout>
         <Header
            style={{
             padding: 24,
             display: 'flex',
-            background: 'yellow',
-            boShadow: '2vh 0vh 2vh rgba(122, 58, 206, 0.2)'
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            background: 'white',
+            boxShadow: '2vh 0vh 2vh rgba(122, 58, 206, 0.2)'
           }}
         >
+          <div>
+            <Button 
+              type='text'
+              className="toogle"
+              onClick={() => setCollapsed(!collapsed)} 
+              icon={collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}
+            />
+          </div>
           <NavSuperior/>
         </Header>
         <Content
