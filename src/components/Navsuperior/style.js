b90e2fb49b687084217c6927e0f49c966a759606
CatianoNorberto <catianonorberto@gmail.com>
2023-10-10 00:56:33 -0300

    responsividade do sidebar e navsuperior

--- src/components/Navsuperior/style.js
+++ src/components/Navsuperior/style.js
@@ -1,13 +1,10 @@
 import styled from "styled-components";
 
 export const NavSuperiorContainer = styled.div`
-  /* background: yellow; */
   width: 100%;
-  /* position: fixed; */
-  /* height: 72px; */
-  /* padding: 24px; */
   display: flex;
   align-items: center;
+  justify-content: flex-end;
 
   ul{
     list-style: none;
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,26 +1,51 @@
+import { useNavigate } from 'react-router-dom';
 import { Menu } from 'antd'
-import './style.css'
-
+import Cookies from 'js-cookie';
 import {  DashboardOutlined, PayCircleOutlined  }from '@ant-design/icons'
 
-export default function NavbarDashboard(){
+import './style.css'
+
+
+export default function NavbarDashboard({ darkTheme }){
+  const navigate = useNavigate();
+  const navegaParaDashboard = () => {
+    navigate('/dashboard');
+  };
+  const navegaParaClientes = () => {
+    navigate('/pessoas');
+  };
+  const handleButtonClickPropostas = () => {
+    navigate('/visualizacaopropostas');
+  };
+  const handleButtonClickPagamentos = () => {
+    navigate('/registropagamentos');
+  };
+
+  const handleButtonClickDeltaHub = () => {
+    navigate('/deltahubbackoffice');
+  };
+
   return(
-    <Menu theme='dark' mode='inline' className='menu-bar'>
-      <Menu.Item key='dashboard' icon={<DashboardOutlined />}>
+    <Menu theme={darkTheme} mode='inline' className='menu-bar'>
+      <Menu.Item key='dashboard' icon={<DashboardOutlined />} onClick={navegaParaDashboard}>
         Dashboard
       </Menu.Item>
-      <Menu.Item key='client' icon={<DashboardOutlined />}>
+      <Menu.Item key='client' icon={<DashboardOutlined />} onClick={navegaParaClientes}>
         Clientes
       </Menu.Item>
-      <Menu.Item key='proposta' icon={<DashboardOutlined />}>
+      <Menu.Item key='proposta' icon={<DashboardOutlined />} onClick={handleButtonClickPropostas}>
         Propostas
       </Menu.Item>
-      <Menu.Item key='pagamentos' icon={<PayCircleOutlined />}>
-        Pagamentos
-      </Menu.Item>
-      <Menu.Item key='deltahubbackoffice' icon={<DashboardOutlined />}>
-        DeltaHub Back Office
-      </Menu.Item>
+      {Cookies.get('usergroup') == 'MASTER' ? (
+        <>
+          <Menu.Item key='pagamentos' icon={<PayCircleOutlined />} onClick={handleButtonClickPagamentos}>
+            Pagamentos
+          </Menu.Item>
+          <Menu.Item key='deltahubbackoffice' icon={<DashboardOutlined />} onClick={() => handleButtonClickDeltaHub}>
+            DeltaHub Back Office
+          </Menu.Item>
+        </>
+      ): null}
     </Menu>
   )
 }
