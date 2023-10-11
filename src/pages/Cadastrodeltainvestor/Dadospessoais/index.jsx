0de623d10728b36441000b98ec1152175092a54c
catianonorbertobdi <catiano.norberto@bdidigital.com.br>
2023-10-11 13:36:46 -0300

    Ajustes de do componente dados pessoais

---
+++ src/pages/Cadastrodeltainvestor/Dadospessoais/index.jsx
@@ -0,0 +1,290 @@
+import { useState } from 'react'
+import { Form, Formik } from 'formik';
+import { 
+  Card, 
+  CardContent, 
+  Typography, 
+  Grid,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+  Checkbox,
+  FormGroup, 
+  TextField
+} from '@mui/material';
+
+import Figure from '../../../imgs/first-steps.svg'
+
+import { 
+  Container, 
+  Content, 
+  CadrastoRight, 
+  CadrastoLeft,
+  CadastroCard,
+  CadastroDeltaContent,
+  Assessor,
+  CardPercent,
+  CardPercentBottom,
+  CardPercentContent,
+  Divider,
+} from './style'
+
+export default function Dadospessoais(){
+  return(
+    <Container>
+      <Content>
+        <CadastroDeltaContent>
+          <CadrastoRight>
+            <CardContent 
+              style={{ padding: '0'}}
+            >
+              <Formik 
+                initialValues={{
+                  nome: '',
+                  cpf: '',
+                  email: '',
+                  telefone: '',
+                  profissao: '',
+                  rendaMedia: '',
+                  valorDesejado: '',
+                  prazo: '',
+                  aceite: ''
+                }}
+                onSubmit={() => {}}
+              >
+                <Form autoComplete='off'>
+                  <Grid container spacing={3} >
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="firstName"
+                        name="firstName"
+                        label="Nome completo (como está no RG)"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="email"
+                        name="email"
+                        label="E-mail"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="confirmar"
+                        name="confirmar"
+                        label="Confirme seu e-mail"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="cpf"
+                        name="cpf"
+                        label="CPF"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="firstName"
+                        name="firstName"
+                        label="Data de nascimento"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="celular"
+                        name="celular"
+                        label="Celular"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="rg"
+                        name="rg"
+                        label="RG"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="orgao"
+                        name="orgao"
+                        label="Orgão emissor"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="estado"
+                        name="estado"
+                        label="Estado onde foi emitido"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="emissao"
+                        name="emissao"
+                        label="Data emissão"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="mae"
+                        name="mae"
+                        label="Nome da mãe"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="pai"
+                        name="pai"
+                        label="Nome Pai (opcional)"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="civil"
+                        name="civil"
+                        label="Estado Civil"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="nacionalidade"
+                        name="nacionalidade"
+                        label="Nacionalidade"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="estadonasceu"
+                        name="estadonasceu"
+                        label="Estado onde nasceu"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        id="cidadenasceu"
+                        name="cidadenasceu"
+                        label="Cidade onde nasceu"
+                        fullWidth
+                        autoComplete="given-name"
+                        variant="standard"
+                      />
+                    </Grid>
+                      <Grid item xs={12} sm={6} sx={{ marginTop: '40px'}}>
+                        <FormControl>
+                          <FormLabel 
+                            style={{ marginBottom: '10px' }}
+                          >Gênero</FormLabel>
+                          <RadioGroup
+                            row
+                            aria-labelledby="demo-row-radio-buttons-group-label"
+                            name="row-radio-buttons-group"
+                          >
+                            <FormControlLabel value="masculino" control={<Radio />} label="Masculino" />
+                            <FormControlLabel value="feminino" control={<Radio />} label="Feminino" />
+                          </RadioGroup>
+                        </FormControl>
+                      </Grid>
+                      <Grid item xs={12} sm={6} sx={{ marginTop: '40px'}}>
+                        <h2 style={{marginBottom: '10px'}}>Declarações</h2>
+                        <FormGroup>
+                          <FormControlLabel control={<Checkbox defaultChecked />} label="Sou vinculado à XP Investimentos" />
+                          <FormControlLabel required control={<Checkbox />} label="Sou Us Person" />
+                          <FormControlLabel required control={<Checkbox />} label="Sou politicamente exposto" />
+                        </FormGroup>
+                      </Grid>
+                  </Grid>
+                </Form>
+              </Formik>
+            </CardContent>
+          </CadrastoRight>
+          <CadrastoLeft>
+            <CadastroCard>
+              <Card sx={{ minWidth: 335 }}>
+                <CardContent sx={{ padding: 4}}>
+                  <img src={Figure} alt="figure" />
+                  <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Você está dando os primeiros passos para abrir uma conta na Delta Investimentos.
+                  </Typography>
+                </CardContent>
+              </Card>
+              
+              <CardPercent>
+                <Divider></Divider>
+                <CardPercentContent>
+                  <img src={Figure} alt="figure" />
+                  <CardPercentBottom>
+                    <p>cadastro</p>
+                    <strong>100% Seguro</strong>
+                  </CardPercentBottom>
+                </CardPercentContent>
+              </CardPercent>
+            </CadastroCard>
+          </CadrastoLeft>
+        </CadastroDeltaContent>
+      </Content>
+    </Container>
+  )
+}
--- src/pages/Cadastrodeltainvestor/style.js
+++ src/pages/Cadastrodeltainvestor/style.js
@@ -16,33 +16,7 @@ export const Content = styled.div`
   >p{
     margin-bottom: 40px;
   }
-  
 `
-export const CadastroDeltaContent = styled.div`
-  gap: 100px;
-  grid-template-columns: 2fr 1fr;
-  display: grid;
-  justify-content: space-between;
-  margin-top: 40px;
-`;
-
-export const CadrastoRight = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-export const CardPercent = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  margin-top: 50px;
-`
-export const Divider = styled.div`
-  width: 24px;
-  height: 4px;
-  /* margin-bottom: 20px; */
-  background: #FFC709;
-`
-
 export const Header = styled.div`
   h2{
     font-size: 32px;
@@ -51,7 +25,7 @@ export const Header = styled.div`
     margin-bottom: 16px;
   }
 
-  p{
+  >p{
     line-height: 1.5;
     font-size: 16px;
     color: rgb(102, 102, 102);
@@ -63,35 +37,4 @@ export const Header = styled.div`
       font-weight: bold;
     }
   }
-`
-export const CardPercentContent = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 20px;
-
-  img{
-    width: 50px;
-    height: 100px;
-  }
-`
-export const CardPercentBottom = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  >p{
-    margin-bottom: 8px;
-  }
-`
-export const CadrastoLeft = styled.div`
-   display: flex;
-   flex-direction: column;
-`
-export const CadastroCard = styled.div`
-
-`
-
-export const Assessor = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 50px;
 `
